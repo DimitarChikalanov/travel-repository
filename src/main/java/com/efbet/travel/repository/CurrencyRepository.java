bae19b35798db0259a6b31a50a1bdeb13b4b8d3f
DimitarChikalanov <dimitur.chikalanov@gmail.com>
2021-04-18 15:31:28 +0300

    update properties

--- src/main/java/com/efbet/travel/repository/CurrencyRepository.java
+++ src/main/java/com/efbet/travel/repository/CurrencyRepository.java
@@ -4,8 +4,6 @@ import com.efbet.travel.domain.entity.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CurrencyRepository extends JpaRepository<Currency,String> {
 
